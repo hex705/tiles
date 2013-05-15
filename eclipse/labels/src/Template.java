c8a74cbf4ab95d017c1c70fb3b73b66595a4e3bc
Kathryn Hartog <kathryn.hartog@gmail.com>
2013-05-15 13:33:04 -0400

    labels first test: interprets data from tiles.xml and prints to console

--- eclipse/labels/src/Template.java
+++ eclipse/labels/src/Template.java
@@ -1,52 +1,35 @@
 import java.util.List;
 
 import processing.core.*;
-import processing.video.Capture;
-import topcodes.*;
+import processing.data.XML;
 
 @SuppressWarnings("serial")
 public class Template extends PApplet {
+	
+	XML xml;
 
-	Capture cam;
-	Scanner scanner;
+	public void setup() {    
 
-	public void setup() {
-		size(640, 480);
-		String[] cameras = Capture.list();
-		cam = new Capture(this, cameras[0]);
-		cam.start();
-		
-		scanner = new Scanner();
+	  xml = loadXML("tiles.xml");
+	  XML[] children = xml.getChildren("tile");
+
+	  for (int i = 0; i < children.length; i++) {
+	    int[] connections = new int[4];
+
+	    int id = children[i].getInt("topcodeID");
+	    String name = children[i].getChild("name").getContent();
+
+	    //connections are in order: LRTB
+	    connections[0] = children[i].getChild("connections").getInt("T"); 
+	    connections[1] = children[i].getChild("connections").getInt("R"); 
+	    connections[2] = children[i].getChild("connections").getInt("B"); 
+	    connections[3] = children[i].getChild("connections").getInt("L"); 
+
+	    println(id + " " + name + "  { L" + connections[3] + "; R" + connections[1] + "; T" + connections[0] + "; B" + connections[2] + " }");
+	  }
 	}
 
-	public void draw() {
-		if (cam.available()) {
-			// Reads the new frame
-			cam.read();
-			
-			// We need a copy -- the scanner modifies the image
-		    int[] pixels = new int[cam.pixels.length];
-		    System.arraycopy(cam.pixels, 0, pixels, 0, cam.pixels.length);
-		    
-		    // Do the work 
-		    List<TopCode> codes = scanner.scan(pixels, cam.width, cam.height);
-		    
-		    // show the results 
-		    println("Codes found: " + codes.size());
-		    image(cam, 0, 0);		    
-		    rectMode(CENTER);
-		    for (TopCode code : codes) {
-		       pushMatrix();
-		       stroke(0, 255, 0);
-		       noFill(); 
-		       translate(code.getCenterX(), code.getCenterY());
-		       text(code.getCode(), 0, 0);
-		       rotate(code.getOrientation());
-		       rect(0, 0, code.getDiameter(), code.getDiameter());		       
-		       popMatrix();
-		    } 
-		} 
-	}
+
 
 	public static void main(String args[]) {
 		PApplet.main(new String[] { "Template" });
