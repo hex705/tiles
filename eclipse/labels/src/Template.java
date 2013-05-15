8d53491aec5b0420ea84d8c874441639d4e133d0
Kathryn Hartog <kathryn.hartog@gmail.com>
2013-05-15 13:50:03 -0400

    Completed tiles.xml to include all known tiles

--- eclipse/labels/src/Template.java
+++ eclipse/labels/src/Template.java
@@ -5,31 +5,31 @@ import processing.data.XML;
 
 @SuppressWarnings("serial")
 public class Template extends PApplet {
-	
+
 	XML xml;
 
-	public void setup() {    
+	public void setup() {
 
-	  xml = loadXML("tiles.xml");
-	  XML[] children = xml.getChildren("tile");
+		xml = loadXML("tiles.xml");
+		XML[] children = xml.getChildren("tile");
 
-	  for (int i = 0; i < children.length; i++) {
-	    int[] connections = new int[4];
+		for (int i = 0; i < children.length; i++) {
+			int[] connections = new int[4];
 
-	    int id = children[i].getInt("topcodeID");
-	    String name = children[i].getChild("name").getContent();
+			int id = children[i].getInt("topcodeID");
+			String name = children[i].getChild("name").getContent();
 
-	    //connections are in order: LRTB
-	    connections[0] = children[i].getChild("connections").getInt("T"); 
-	    connections[1] = children[i].getChild("connections").getInt("R"); 
-	    connections[2] = children[i].getChild("connections").getInt("B"); 
-	    connections[3] = children[i].getChild("connections").getInt("L"); 
+			// connections are in order: TRBL
+			connections[0] = children[i].getChild("connections").getInt("T");
+			connections[1] = children[i].getChild("connections").getInt("R");
+			connections[2] = children[i].getChild("connections").getInt("B");
+			connections[3] = children[i].getChild("connections").getInt("L");
 
-	    println(id + " " + name + "  { L" + connections[3] + "; R" + connections[1] + "; T" + connections[0] + "; B" + connections[2] + " }");
-	  }
+			println(id + " " + name + "  { L" + connections[3] + "; R"
+					+ connections[1] + "; T" + connections[0] + "; B"
+					+ connections[2] + " }");
+		}
 	}
-
-
 
 	public static void main(String args[]) {
 		PApplet.main(new String[] { "Template" });
