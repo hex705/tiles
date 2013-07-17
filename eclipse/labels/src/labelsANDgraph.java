2370c69704d3d98d890e77c45bf8caaeccd2a260
Kathryn Hartog <kathryn.hartog@gmail.com>
2013-07-17 13:49:16 -0400

    restructured Tile class for easier traversal

--- eclipse/labels/src/labelsANDgraph.java
+++ eclipse/labels/src/labelsANDgraph.java
@@ -59,6 +59,7 @@ public class labelsANDgraph extends PApplet {
 
 			int id = children[i].getInt("topcodeID");
 			String name = children[i].getChild("name").getContent();
+			String type = children[i].getString("type");
 
 			// connections are in order: TRBL
 			connections[0] = children[i].getChild("connections").getInt("T");
@@ -70,7 +71,7 @@ public class labelsANDgraph extends PApplet {
 			//		+ connections[1] + "; T" + connections[0] + "; B"
 			//		+ connections[2] + " }");
 			// pass all data to appropriate tile object
-			tileset[i] = new Tile (id, name, connections[0], connections[1], connections[2], connections[3]);
+			tileset[i] = new Tile (id, name, type, connections[0], connections[1], connections[2], connections[3]);
 		}
 		for (int i = 0; i < tileset.length; i ++) {
 			println (tileset[i].topcodeID + " " + tileset[i].topcodeName + "  { L" + tileset[i].connections[3] + "; R"
@@ -275,13 +276,15 @@ public class labelsANDgraph extends PApplet {
 		int[] connections;
 		int topcodeID;
 		String topcodeName;
+		String type;
 		
 		float centerX, centerY, diameter;
 		
-		Tile(int _topcodeID, String _topcodeName, int c0, int c1, int c2, int c3) {
+		Tile(int _topcodeID, String _topcodeName, String _type, int c0, int c1, int c2, int c3) {
 			connections = new int[4];
 			topcodeID = _topcodeID;
 			topcodeName = _topcodeName;
+			type = _type;
 	
 			// connections are in order: TRBL
 			connections[0] = c0;
@@ -291,11 +294,11 @@ public class labelsANDgraph extends PApplet {
 		}
 		@Override
 		public String toString() {
-			return str(this.topcodeID) + " " + this.topcodeName + " ( " + this.centerX + ", " + this.centerY + ", " + this.diameter + ")";
+			return str(this.topcodeID) + " " + this.topcodeName + " (" + this.type + ") ( " + this.centerX + ", " + this.centerY + ", " + this.diameter + ")";
 		}
 	}
 
 	public static void main(String args[]) {
-		PApplet.main(new String[] { "labelsANDgraph" }); 
+		PApplet.main(new String[] { "labelsANDgraph" });
 	}
 }
